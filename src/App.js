6e906822240339199fb04be8fb93b989d5151c73
murtuza <murtuza.creativity@gmail.com>
2021-12-22 18:57:47 +0530

    improved frontend

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
   return (
     <div className="App">
       <h1>Counter</h1>
+      <h4>What's the number of count?</h4>
       <div className="input_field">
         <input value={input} onChange={e => setInput(e.target.value)} className="no_of_times" type="text" />
         <button onClick={register}>Submit</button>
